33914464943b804629eab7f45990622c5d1cdf90
Frelin <fogariojohnson@gmail.com>
2023-07-18 04:00:56 -0500

    Responsiveness and authenticating are added.  Docstring is also added.

--- datamanager/json_data_manager.py
+++ datamanager/json_data_manager.py
@@ -1,27 +1,113 @@
 import json
 import requests
 from .data_manager_interface import DataManagerInterface
-
+import imdb
+from werkzeug.security import generate_password_hash
 
 API_KEY = "e2e17332"
 
 
 class JSONDataManager(DataManagerInterface):
+    """JSONDataManager class represents a storage for the JSON file"""
     def __init__(self, filename):
+        """
+        Initializes a new instance of the JSON file.
+
+        Arg:
+            filename(file path): The location of the JSON file.
+        """
         self.filename = filename
+        self.country_dict = {}
 
     def get_all_users(self):
+        """
+        Gets all the users from the JSON file.
+
+        Return:
+            data(list): List of all the users from the JSON file
+        """
         with open(self.filename, 'r') as file_obj:
             data = json.load(file_obj)
             return list(data.values())
 
     def get_user_movies(self, user_id):
+        """
+        Gets all the movies of the user from the JSON file.
+
+        Arg:
+            user_id(str): The unique key or user id of the user.
+
+        Return:
+            user(dict): A dictionary of the movies of the indicated user.
+        """
         with open(self.filename, 'r') as file_obj:
             data = json.load(file_obj)
             user = data.get(user_id)
             return user
 
-    def add_user(self, name):
+    def get_user(self, user_id):
+        """
+        Gets all the  information of the user from the JSON file.
+
+        Arg:
+            user_id(str): The unique key or user id of the user.
+
+        Return:
+            user(dict): A dictionary of the information of the user.
+        """
+        with open(self.filename, 'r') as file:
+            data = json.load(file)
+            users = data.get('users', [])
+            for user in users:
+                if user['user_id'] == user_id:
+                    return user
+        return None
+
+    def get_user_by_username(self, username):
+        """
+        Gets all the  information of the user using its username from the JSON file.
+
+        Arg:
+            username(str): The username of the user as indicated in the JSON file.
+
+        Return:
+            user(dict): A dictionary of the information of the user.
+        """
+        with open(self.filename, 'r') as file_obj:
+            data = json.load(file_obj)
+            for user_id, user in data.items():
+                if user.get("username") == username:
+                    user["user_id"] = user_id
+                    return user
+        return None
+
+    def authenticate_user(self, username, password):
+        """
+        Authenticate the user by its username and password information as stored in the JSON file.
+
+        Arg:
+            username(str): The username of the user as indicated in the JSON file.
+            password((str): The password of the user as indicated in the JSON file.
+
+        Return:
+            user(dict): A dictionary of the information of the user.
+        """
+        user = self.get_user_by_username(username)
+        if user and user.get('password') == password:
+            return user
+        return None
+
+    def add_user(self, name, username, password):
+        """
+        Allows user to add another user.
+
+        Arg:
+            name(str): The name of the user which will be used in the JSON file
+            username(str): The username of the user which will be used ito access the movies in the JSON file.
+            password(str): The password of the user which will be used to access the movies in the JSON file.
+
+        Return:
+            user_id(dict): A dictionary of the information of the user."""
         try:
             with open(self.filename, 'r') as file_obj:
                 data = json.load(file_obj)
@@ -30,9 +116,11 @@ class JSONDataManager(DataManagerInterface):
                     if user['name'] == name:
                         return None
 
-                user_id = str(len(data) + 1)
+                user_id = str(len(data) + 1)  # Generate a unique user ID
                 user = {
                     "name": name,
+                    "username": username,
+                    "password": password,
                     "movies": {}
                 }
                 data[user_id] = user
@@ -45,6 +133,42 @@ class JSONDataManager(DataManagerInterface):
             print(f'Error: {e}')
             return None
 
+    def get_country_code(self):
+        """ Creates the country code.
+
+        Returns:
+            country_dict(dict): A dictionary of countries and its code
+        """
+        api_key_holiday = "a47a88cb-3005-44d5-99d8-81710c7975cb"
+        country_list = requests.get(f"https://holidayapi.com/v1/countries?pretty&key={api_key_holiday}")
+        res = country_list.json()
+        countries = res["countries"]
+        dictionary = {}
+        for country in countries:
+            country_entry = country['name']
+            code_entry = country['code']
+            dictionary[country_entry] = code_entry
+        self.country_dict = dictionary
+        return self.country_dict
+
+    @staticmethod
+    def genre_maker(genre):
+        """ Creates a genre image of the movie
+
+        Args:
+            genre(str): Emoticon to display
+
+        Returns:
+            character(str): A link to the character image
+        """
+        api_key = "RRhRnIM0DDVn6BbmwULBzQ==n9DVxWtNtkN2nwH5"
+        api_url = 'https://api.api-ninjas.com/v1/emoji?name={}'.format(genre)
+        response = requests.get(api_url, headers={'X-Api-Key': api_key})
+        list_options = response.json()
+        for char in list_options:
+            character = char["image"]
+        return character
+
     @staticmethod
     def fetch_movie_details(title):
         url = f"http://www.omdbapi.com/?apikey={API_KEY}&t={title}"
@@ -55,7 +179,64 @@ class JSONDataManager(DataManagerInterface):
         return None
 
     def add_movie(self, user_id, title):
+        """
+        Adds a movie to the movies' database.
+        Loads the information from the JSON file, add the movie,
+        and saves it.
+
+        Args:
+            user_id(str): The unique id of the user.
+            title(str): Title of the movie to be added.
+
+        Raises:
+            Exception: If the user id is not found
+            Exception: Covers all the other error that might occur and explains user the reason.
+        """
         movie_data = self.fetch_movie_details(title)
+
+        # API for omdb
+        url_omd = f"http://www.omdbapi.com/?apikey={API_KEY}&t="
+
+        # To locate the URL of the added movie
+        ia = imdb.IMDb()
+        add_movie = ia.search_movie(title)
+        movie = add_movie[0]
+        ia.update(movie)
+        imdb_url = ia.get_imdbURL(movie)
+
+        # To create the flag image of the added movie
+        add_url = url_omd + title
+        movie = requests.get(add_url)
+        res = movie.json()
+        country = res["Country"]
+        if "," in country:
+            country = country.split(",")[0]
+        country_code = self.get_country_code()
+        country_origin = country_code[country]
+
+        # To create the genre image of the added movie
+        genre_source = res["Genre"]
+        first_genre = genre_source.split(",")[0]
+        dict_of_genre_emoticons = {
+            "Horror": "vampire",
+            "Action": "bomb",
+            "Western": "cowboy hat face",
+            "Adventure": "person climbing",
+            "Animation": "mage",
+            "Comedy": "clown face",
+            "Crime": "kitchen knife",
+            "Drama": "loudly crying face",
+            "Fantasy": "fairy",
+            "Mystery": "detective",
+            "Romance": "kiss",
+            "Science Fiction": "alien",
+            "Thriller": "ogre",
+            "War": "crossed swords"
+        }
+        for key in dict_of_genre_emoticons.keys():
+            if key == first_genre:
+                genre = dict_of_genre_emoticons[key]
+                movie_genre = self.genre_maker(genre)
         if movie_data:
             try:
                 with open(self.filename, 'r') as file_obj:
@@ -69,7 +250,11 @@ class JSONDataManager(DataManagerInterface):
                             'title': movie_data.get('Title'),
                             'director': movie_data.get('Director'),
                             'year': movie_data.get('Year'),
-                            'rating': movie_data.get('imdbRating')
+                            'rating': movie_data.get('imdbRating'),
+                            'poster': movie_data.get('Poster'),
+                            'genre': movie_genre,
+                            'flag': f'https://flagsapi.com/{country_origin}/shiny/64.png',
+                            'url': imdb_url
                         }
 
                         # Check if the movie already exists in the user's movies
@@ -87,6 +272,19 @@ class JSONDataManager(DataManagerInterface):
                 print(f'Error: {e}')
 
     def update_movie(self, user_id, movie_id, title, director, year, rating):
+        """
+        Updates a movie from the movies' database.
+        Loads the information from the JSON file, updates the movie,
+        and saves it. The function doesn't need to validate the input.
+
+        Args:
+            user_id(str): The unique id of the user.
+            movie_id(str): The movie id of the user's movie.
+            title(str): Movie title to update.
+            director(str): Director to update.
+            year(int): Year to update.
+            rating(float): Rating to update.
+        """
         with open(self.filename, 'r') as file_obj:
             data = json.load(file_obj)
             user = data.get(user_id)
@@ -102,6 +300,16 @@ class JSONDataManager(DataManagerInterface):
             json.dump(data, file_obj, indent=4)
 
     def delete_movie(self, user_id, movie_id):
+        """
+        Deletes a movie from the movies' database.
+        Loads the information from the JSON file, deletes the movie,
+        and saves it.
+
+        Args:
+            user_id(str): The unique id of the user.
+            movie_id(str): The movie id of the user's movie.
+
+        """
         with open(self.filename, 'r') as file_obj:
             data = json.load(file_obj)
             user = data.get(user_id)
@@ -113,6 +321,14 @@ class JSONDataManager(DataManagerInterface):
             json.dump(data, file_obj, indent=4)
 
     def delete_user(self, user_id):
+        """
+        Deletes the user from the database.
+        Loads the information from the JSON file, deletes the movie,
+        and saves it.
+
+        Args:
+            user_id(str): The unique id of the user.
+        """
         with open(self.filename, 'r') as file_obj:
             data = json.load(file_obj)
             if str(user_id) not in data:
@@ -120,6 +336,3 @@ class JSONDataManager(DataManagerInterface):
             del data[str(user_id)]
         with open(self.filename, 'w') as file_obj:
             json.dump(data, file_obj, indent=4)
-
-
-
--- json_converter.py
+++ json_converter.py
@@ -8,13 +8,21 @@ users = {
                 "title": "Inception",
                 "director": "Christopher Nolan",
                 "year": 2010,
-                "rating": 8.8
+                "rating": 8.8,
+                "poster": "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+                "genre": "https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F47D.png",
+                "flag": "https://flagsapi.com/US/shiny/64.png",
+                "url": "https://www.imdb.com/title/tt1375666/?ref_=nv_sr_srsg_0_tt_8_nm_0_q_Incep"
             },
             2: {
                 "title": "The Dark Knight",
                 "director": "Christopher Nolan",
                 "year": 2008,
-                "rating": 9.0
+                "rating": 9.0,
+                "poster": "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+                "genre": "https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F4A3.png",
+                "flag": "https://flagsapi.com/US/shiny/64.png",
+                "url": "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg"
             }
         }
     },
@@ -22,16 +30,24 @@ users = {
         "name": "Bob",
         "movies": {
             1: {
-                "title": "John Wick: Chapter 4",
-                "director": "Chad Stahelski",
-                "year": 2023,
-                "rating": 7.9
+                "title": "Star Wars: Episode V",
+                "director": "Irvin Kershner",
+                "year": 1980,
+                "rating": 8.7,
+                "poster": "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+                "genre": "https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F4A3.png",
+                "flag": "https://flagsapi.com/US/shiny/64.png",
+                "url": "https://www.imdb.com/title/tt0080684/?ref_=nv_sr_srsg_4_tt_7_nm_0_q_Star%2520Wars%253A%2520Episode%2520V"
             },
             2: {
-                "title": "Avengers: Endgame",
-                "director": "Anthony and Joe Russo",
-                "year": 2019,
-                "rating": 8.4
+                "title": "Titanic",
+                "director": "James Cameron",
+                "year": 1997,
+                "rating": 7.9,
+                "poster": "https://m.media-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg",
+                "genre": "https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F62D.png",
+                "flag": "https://flagsapi.com/US/shiny/64.png",
+                "url": "https://www.imdb.com/title/tt0120338/"
             }
         }
     }
